40fe8e0d7aa7339a3807705b1fe5fdf49fdd61cb
MartkCz <antik494@gmail.com>
2019-04-29 11:13:56 +0200

    fixed exception

--- src/Exceptions/ComponentCreationException.php
+++ src/Exceptions/ComponentCreationException.php
@@ -6,24 +6,24 @@ use Throwable;
 
 class ComponentCreationException extends \Exception {
 
-	public static function createJson(\ReflectionProperty $property, \Throwable $previous): self {
+	public static function createJson(\ReflectionProperty $property, Throwable $previous): self {
 		return self::createInvalidAnnotation('invalid json: ' . $previous->getMessage(), $property, $previous);
 	}
 
 	public static function createInvalidAnnotation(string $message, \ReflectionProperty $property, ?Throwable $previous = null): self {
-		$message = sprintf('Invalid annotation in property %s::$%s : %s',
+		$message = sprintf('Invalid annotation in property %s::$%s: %s',
 			$property->getDeclaringClass()->getName(), $property->getName(), $message
 		);
 
-		return new self($message, $property, $previous);
+		return new self($message, 0, $previous);
 	}
 
 	public static function createInvalidProperty(string $message, \ReflectionProperty $property, ?Throwable $previous = null): self {
-		$message = sprintf('Invalid property %s::$%s : %s',
+		$message = sprintf('Invalid property %s::$%s: %s',
 			$property->getDeclaringClass()->getName(), $property->getName(), $message
 		);
 
-		return new self($message, $property, $previous);
+		return new self($message, 0, $previous);
 	}
 
 }
