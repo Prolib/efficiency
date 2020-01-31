818b5bb6561817a47ff5807d6ebc620f05923451
MartkCz <martk494@gmail.com>
2020-01-31 10:20:06 +0100

    FormUtils: onError accepts only one argument

--- src/Utils/FormUtils.php
+++ src/Utils/FormUtils.php
@@ -38,6 +38,10 @@ class FormUtils {
 		$this->control = $control;
 	}
 
+	public static function create(Form $form, Control $control): self {
+		return new self($form, $control);
+	}
+
 	public function flashMessage(string $message) {
 		$this->onSuccess[] = function () use ($message) {
 			$this->control->flashMessage($message);
@@ -107,9 +111,9 @@ class FormUtils {
 	/**
 	 * @internal
 	 */
-	public function __onError(Form $form, array $values): void {
+	public function __onError(Form $form): void {
 		foreach ($this->onError as $error) {
-			$error($values);
+			$error($form);
 		}
 	}
 
