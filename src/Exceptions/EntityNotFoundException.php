5e90c946d761aabbdae7f9fbe6f6ba05a099a532
MartkCz <martk494@gmail.com>
2020-01-31 10:03:58 +0100

    EntityNotFoundException extends BadRequestException

--- src/Exceptions/EntityNotFoundException.php
+++ src/Exceptions/EntityNotFoundException.php
@@ -2,9 +2,10 @@
 
 namespace ProLib\Efficiency\Exceptions;
 
+use Nette\Application\BadRequestException;
 use Throwable;
 
-class EntityNotFoundException extends \RuntimeException {
+class EntityNotFoundException extends BadRequestException {
 
 	public function __construct(string $class, $id) {
 		parent::__construct("Entity $class($id) not found.");
